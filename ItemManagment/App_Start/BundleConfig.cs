415fc96a009144efaf958633731405a6670a2cc6
Szczepan <szczep.kowalik@gmail.com>
2022-10-17 20:32:17 +0200

    Basic Views

--- ItemManagment/App_Start/BundleConfig.cs
+++ ItemManagment/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace ItemManagement
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/CSS/bootstrap.css",
+                      "~/Content/CSS/site.css"));
         }
     }
 }
